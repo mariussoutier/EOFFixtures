72c42d5c4e55eb106e3c6c60b48581c4d5b6403f
Marius Soutier <spybreak@gmx.net>
2011-07-04 10:31:51 +0200

    Prepare fixtures spec

---
+++ Tests/de/soutier/fixtures/FixturesSpec.scala
@@ -0,0 +1,16 @@
+package de.soutier.fixtures
+import org.specs.SpecificationWithJUnit
+
+class FixturesSpec extends SpecificationWithJUnit {
+  "Loading a YAML fixture" should {
+    "map to the correct enterprise objects" in {
+      // TODO Create a demo EO and load it from YAML
+    }
+  }
+  
+  "Dumping enterprise objects to YAML" should {
+    "map the getters correctly" in {
+      // TODO
+    }
+  }
+}
